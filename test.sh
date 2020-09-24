a054a01d91e4ff65e6eb9a2d25059d2a5b716c02
Marcel Martin <marcel.martin@scilifelab.se>
2020-09-24 14:10:26 +0200

    Use same test script locally and on CI

--- test.sh
+++ test.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
+version="0.8"
+
+testdata_file=minute-testdata-${version}.tar.gz
+if ! [[ -f ${testdata_file} ]]; then
+  wget -nv https://export.uppmax.uu.se/snic2020-6-3/minute-testdata/${testdata_file}
+fi
+
 rm -rf testrun
 mkdir testrun
 cd testrun
-cp ../minute-testdata/* .
-mkdir fastq
-mv *.fastq.gz fastq/
-bowtie2-build ref.fa.gz ref
+tar --strip-components=1 -xf ../${testdata_file}
+cp ../testdata/*.{tsv,bed,sizes,yaml} .
+mkdir fastq && mv -- *.fastq.gz fastq/
+mkdir ref && mv ref.* ref/
+( cd ref && bowtie2-build ref.fa.gz ref )
+
 snakemake -p -j 1 -s ../Snakefile "$@"
