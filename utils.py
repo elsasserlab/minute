5f36203d7b854e77b659bcefb24e51f364c89978
C. Navarro <cnluzon@gmail.com>
2020-09-25 10:59:04 +0200

    scaling factor calculated per scaling group

--- utils.py
+++ utils.py
@@ -42,7 +42,8 @@ class TreatmentControlPair:
     treatment: Library
     control: Library
     scaling_group: int
-
+    treatment_path: str = None
+    control_path: str = None
 
 def read_libraries():
     for row in read_tsv("experiment.tsv", columns=4):
@@ -144,31 +145,53 @@ def parse_picard_metrics(path, metrics_class: str):
     result = {key.lower(): float_or_int(value) for key, value in zip(header, values)}
     return result
 
+def compute_reference_pair_scaling_factors(normalization_pairs, genome_size, fragment_size):
+    scaling_factors = {}
+    library_scale_groups = map_scaling_group_to_list_of_normalization_pairs(normalization_pairs)
+
+    for scaling_group in library_scale_groups:
+        reference_pair = library_scale_groups[scaling_group][0]
+
+        treatment_reads = flagstat_mapped_reads(reference_pair.treatment_path)
+        control_reads = flagstat_mapped_reads(reference_pair.control_path)
+        scaling_factors[scaling_group] = (
+                genome_size / fragment_size / treatment_reads * control_reads
+            )
+
+    return scaling_factors
+
+def map_scaling_group_to_list_of_normalization_pairs(normalization_pairs):
+    scaling_group_to_normalization_pairs = defaultdict(list)
+    for pair in normalization_pairs:
+        scaling_group_to_normalization_pairs[pair.scaling_group].append(pair)
+    return scaling_group_to_normalization_pairs
 
 def compute_scaling(normalization_pairs, treatments, controls, infofile, genome_size, fragment_size):
     print("sample_name", "#reads", "n_scaled_reads", "input_name", "n_input_reads", "factor", sep="\t", file=infofile)
-    first = True
-    scaling_factor = -1
+
+    for pair, treatment_path, control_path in zip(normalization_pairs, treatments, controls):
+        pair.treatment_path = treatment_path
+        pair.control_path = control_path
+
+    reference_pair_scaling_factors = compute_reference_pair_scaling_factors(
+        normalization_pairs,
+        genome_size,
+        fragment_size)
+
     for pair, treatment_path, control_path in zip(normalization_pairs, treatments, controls):
         treatment_reads = flagstat_mapped_reads(treatment_path)
         control_reads = flagstat_mapped_reads(control_path)
-        if first:
-            scaling_factor = (
-                genome_size / fragment_size / treatment_reads * control_reads
-            )
-            treatment_reads_ref = treatment_reads
-            control_reads_ref = control_reads
-            first = False
 
-        sample_scaling_factor = scaling_factor / control_reads
+        sample_scaling_factor = reference_pair_scaling_factors[pair.scaling_group] / control_reads
         scaled_treatment_reads = sample_scaling_factor * treatment_reads
 
         # TODO factor this out
         print(pair.treatment.name, treatment_reads, scaled_treatment_reads, pair.control.name, control_reads, sample_scaling_factor, sep="\t", file=infofile)
 
         # TODO scaled.idxstats.txt file
-
         yield sample_scaling_factor
+
+
 
 
 def parse_stats_fields(stats_file):
